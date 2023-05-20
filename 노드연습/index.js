987460a1f495eeb720351531e7679bd6f0abaf70
dabinnkim <dabin6743@gmail.com>
2023-05-20 18:26:54 +0900

    axios로 입력값 새로고침 없이 출력

---
+++ 노드연습/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.get("/axios", function (req, res) {
+  console.log(req.query); //콘솔창 출력
+  res.send(req.query); //화면에 출력
+});
+
+app.listen(PORT, function () {
+  console.log(`http://localhost:${PORT}`);
+});
