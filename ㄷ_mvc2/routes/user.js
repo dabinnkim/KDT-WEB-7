734eea2fcb12a8cfc85fbe5c936a024525aecdff
dabinnkim <dabin6743@gmail.com>
2023-05-26 00:17:33 +0900

    라우터, index.js 경로 지정

--- ㄷ_mvc2/routes/user.js
+++ ㄷ_mvc2/routes/user.js
@@ -2,6 +2,8 @@ const express = require("express");
 const controller = require("../controller/user"); //컨트롤러에서 user 함수 가져옴
 const router = express.Router();
 
-router.get("/", controller.user); // 여기서 왜 / 이고 인덱스에서 /user인지 모르겠음
+router.get("/", controller.user);
+// 위의 경로에서 왜 / ? index.js에서 /user인지 모르겠음
+//=> index.j의 /user + 여기의 경로 / 가 합쳐지는 것!
 
 module.exports = router;
