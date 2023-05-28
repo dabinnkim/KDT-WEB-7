db050c58c99bb9b144b9d8c67c6bb3151e6bd788
dabinnkim <dabin6743@gmail.com>
2023-05-28 23:18:13 +0900

    mvc 패턴 + mysql

--- ㄷ_mvc_mysql/controller/visitor.js
+++ ㄷ_mvc_mysql/controller/visitor.js
@@ -4,8 +4,8 @@ exports.main = (req, res) => {
   res.render("index");
 };
 
+//모델에서 받은 db 전체 데이터 뷰로 보내기
 exports.getVisitors = (req, res) => {
-  //res.render("visitor", { data: visitor.getVisitors() });
   //visitor.getVisitor()는 model에서 가져온 데이터, 모델에서 가져온 데이터는 모델에서 만든 콜백함수를 통해 전달됨
   visitor.getVisitors((result) => {
     console.log(result); //result가 모델에서 callback으로 넘겨준 rows(성공)값
@@ -15,17 +15,27 @@ exports.getVisitors = (req, res) => {
 
 //하나씩 쿼리스트링 *쿼리스트링이란? url에서 ? 뒤에오는 값들 key:value 형태롱
 exports.CgetVisitor1 = (req, res) => {
-  console.log(req.query);
+  console.log(req.query); //req.query가 사용자가 입력한 쿼리스트링 데이터를 가져옴
   visitor.getVisitors1(req.query.id, (value) => {
-    res.render("visitor", { data: value });
+    res.render("visitor", { data: value }); //ejs가 보내는 방식
+    //응답.send() : 클라이언트에 응답 데이터를 보낼때 사용 .send(),render() 보통 둘중 하나만씀
+    // res.send(value); //데이터를 보내는 방식?
+  });
+};
+
+//하나씩 params
+exports.CgetVisitor2 = (req, res) => {
+  //model에서 만든 모듈 실행
+  console.log(req.params);
+  visitor.getVisitors1(req.params.id, (value) => {
+    res.send(value);
   });
 };
 
 //모델의 데이터
 exports.postVisitor = (req, res) => {
   console.log(req.body);
-
-  //모델에서 만든 post value가 콜백함수 받아온것
+  //모델에서 만든 post value의 콜백함수 받아온것
   visitor.postVisitor(req.body, (value) => {
     //데이터를 프론트에 보내줌
     res.send({
@@ -34,5 +44,23 @@ exports.postVisitor = (req, res) => {
       name: req.body.name,
       comment: req.body.comment,
     });
+  });
+};
+
+//수정
+//get빼고는 req.body??
+exports.CpatchVisitor = (req, res) => {
+  console.log(req.body);
+  //모델에서 만든 모듈 실행
+  visitor.MpatchVisitor(req.body, () => {
+    res.send({ result: true }); //reulst:true 나 데이터 전송 성공했다 알리는것
+  });
+};
+
+//삭제
+exports.CdeleteVisitor = (req, res) => {
+  //모델에서 만든 모듈
+  visitor.MdeleteVisitor(req.body.id, (value) => {
+    res.send({ result: value });
   });
 };
--- ㄷ_mvc_mysql/routes/visitor.js
+++ ㄷ_mvc_mysql/routes/visitor.js
@@ -2,16 +2,25 @@ const express = require("express");
 const controller = require("../controller/visitor");
 const router = express.Router();
 
-//GET localhost : 8000/visitor/
+//GET localhost:8000/visitor/
 router.get("/", controller.main); // /visitor + /
 
-//GET localhost : 8000/visitor/visitors
+//GET localhost:8000/visitor/visitors
 router.get("/visitors", controller.getVisitors);
 
 //GET localhost:8000/visitor/get?id=N
 router.get("/get", controller.CgetVisitor1);
 
-//POST localhost : 8000/visitor/write
+//GET localhost:8000/visitor/:id
+router.get("/:id", controller.CgetVisitor2); //콘솔창에 { id: '3' } 형식으로 출력
+
+//POST localhost:8000/visitor/write
 router.post("/write", controller.postVisitor);
+
+//PATCH localhost:8000/visitor/edit
+router.patch("/edit", controller.CpatchVisitor);
+
+//DELTE localhost:8000/visitor/delete
+router.delete("/delete", controller.CdeleteVisitor);
 
 module.exports = router;
