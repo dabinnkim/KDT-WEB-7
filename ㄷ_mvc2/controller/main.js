e5cce6508ed94e30c20ce2df2929f4869194e637
dabinnkim <dabin6743@gmail.com>
2023-05-28 19:01:28 +0900

    req.params로 url 파라미터 값 가져오기

--- ㄷ_mvc2/controller/main.js
+++ ㄷ_mvc2/controller/main.js
@@ -12,7 +12,7 @@ exports.comments = (req, res) => {
 
 //comment 함수 실행시 comment.ejs 파일 렌더링 + 데이터 전송
 exports.comment = (req, res) => {
-  const commentId = req.params.page; //url의 파라미터값을 가져옴
+  const commentId = req.params.page; //url의 파라미터값을 가져옴, 라우트에서 /:page 이부분이 들어오는 것
   const comments = comment.commentInfos(); //모델에서 가져온 가상데이터
 
   if (commentId < 1 || commentId > comments.length) {
