e8f41adbea2fb3622b649f86e5617958a6ad1fc6
dabinnkim <dabin6743@gmail.com>
2023-05-31 21:50:28 +0900

    쿠키를 이용해 오늘 하루 그만보기 모달창 만들기

---
+++ ㄷ_쿠키연습/cookie.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const app = express();
+const PORT = 8000;
+
+app.use(cookieParser());
+const cookieOption = {
+  maxAge: 60 * 1000,
+};
+
+//템플릿
+app.set("view engine", "ejs");
+app.use("/view", express.static(__dirname + "/views"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+//axios에서 post
+app.post("/setCookie", (req, res) => {
+  res.cookie("popup", "hide", cookieOption);
+  res.send({ result: true });
+});
+
+app.get("/clearCookie", (req, res) => {
+  //res.clearCookie(키,값,옵션)
+  //키,값,옵션이 생성한것과 동일해야함, maxAge는 일치할 필요 업음
+  res.clearCookie("popup", "hide", cookieOption);
+  res.send({ result: "clear" });
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
